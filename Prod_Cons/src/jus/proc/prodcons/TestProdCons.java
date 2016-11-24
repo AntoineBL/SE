4452ac6cc08218c043f73310f5d34414a7d65c5d
DESKTOP-OAANS63\Djypzi <antoineblanc269@laposte.net>
2016-11-24 16:45:54 +0100

    ajout de class

---
+++ Prod_Cons/src/jus/proc/prodcons/TestProdCons.java
@@ -0,0 +1,19 @@
+package jus.proc.prodcons;
+
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons.Simulateur;
+
+public class TestProdCons extends Simulateur{
+
+	public TestProdCons(Observateur observateur) {
+		super(observateur);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void run() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
