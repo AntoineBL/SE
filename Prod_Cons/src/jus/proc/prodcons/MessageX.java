8890f96ca623fdf47853c80e4d1369b394b8dd26
fustesr <raphael.fustes@gmail.com>
2016-12-02 09:11:24 +0100

    Classe message + debut producteur

--- Prod_Cons/src/jus/proc/prodcons/MessageX.java
+++ Prod_Cons/src/jus/proc/prodcons/MessageX.java
@@ -1,7 +1,25 @@
 package jus.proc.prodcons;
 
 import jus.poc.prodcons.Message;
+import jus.poc.prodcons._Producteur;
 
 public class MessageX implements Message{
 
+	// Producteur du message
+	private Producteur mProducteur;
+	
+	// ID du message
+	private int IDmessage;
+	
+	public MessageX(Producteur producteur, int IDmessage) {
+		
+		this.mProducteur = producteur;
+		this.IDmessage = IDmessage;
+		
+	}
+	
+	public String toString() {
+		
+		return "Message NO: " + IDmessage + "  // Producteur du message: " + mProducteur.identification();
+	}
 }
--- Prod_Cons/src/jus/proc/prodcons/Producteur.java
+++ Prod_Cons/src/jus/proc/prodcons/Producteur.java
@@ -1,16 +1,25 @@
 package jus.proc.prodcons;
 
 import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
 import jus.poc.prodcons.ControlException;
 import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons._Producteur;
 
 public class Producteur extends Acteur implements _Producteur{
 
-	protected Producteur(int type, Observateur observateur, int moyenneTempsDeTraitement,
+	//Buffer utilisé par le producteur
+	private ProdCons buffer;
+	
+	//Nombre de message du producteur
+	private int nbMessage;
+	
+	protected Producteur(ProdCons buffer, int type, Observateur observateur, int moyenneTempsDeTraitement,
 			int deviationTempsDeTraitement) throws ControlException {
 		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
-		// TODO Auto-generated constructor stub
+		
+		this.buffer = buffer;
+		this.nbMessage = Aleatoire.valeur(moyenneTempsDeTraitement, deviationTempsDeTraitement);
 	}
 
 	@Override
